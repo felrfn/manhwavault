a4526bdb9cc3eaa9e76c0aa7419d4f1dca0cbc5f
felrfn <ardiansyahrafael5@gmail.com>
2025-11-21 11:13:59 +0700

    init backend & database model

---
+++ apps/backend/config/env.ts
@@ -0,0 +1,7 @@
+import 'dotenv/config';
+
+export const env = {
+  PORT: process.env.PORT ? Number(process.env.PORT) : 4000,
+  DATABASE_URL: process.env.DATABASE_URL ?? '',
+  JWT_SECRET: process.env.JWT_SECRET ?? 'dev_secret_change_me'
+};
